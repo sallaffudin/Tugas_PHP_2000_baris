4bbcdaeecc71eb2dc416cc6d0b7a5a6d89594b35
broadji <adjipangestu.id@gmail.com>
2023-01-10 22:32:04 +0700

    3. migrate sample param tabel assets

--- database/migrations/2023_01_10_145914_create_assets_table.php
+++ database/migrations/2023_01_10_145914_create_assets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateAssetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('assets', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('jenis');
+            $table->integer('harga');
+            $table->string('status', 20);
             $table->timestamps();
         });
     }
@@ -28,4 +32,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('assets');
     }
-};
+}
